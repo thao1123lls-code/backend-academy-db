fabf6ba409d95bc208e820b3cda8edb378ee06e0
thao1123lls-code <thao1.123lls@gmail.com>
2026-04-10 09:35:56 +0700

    feat: complete Phase 1 - Day 5 Relationship

--- schema.sql
+++ schema.sql
@@ -12,3 +12,26 @@ CREATE TABLE products (
     stock INTEGER NOT NULL CHECK (stock >= 0),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+ CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    total_amount NUMERIC(12,2) NOT NULL CHECK (total_amount >= 0),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_userFOREIGN KEY(user_id)REFERENCES users(id)
+);
+ CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    full_name VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE order_items (
+    id SERIAL PRIMARY KEY,
+    order_id INTEGER NOT NULL,
+    product_id INTEGER NOT NULL,
+    quantity INTEGER NOT NULL CHECK (quantity > 0),
+    price NUMERIC(10,2) NOT NULL CHECK (price > 0),
+    CONSTRAINT fk_order FOREIGN KEY(order_id)REFERENCES orders(id),
+    CONSTRAINT fk_productFOREIGN KEY(product_id)REFERENCES products(id)
+);
